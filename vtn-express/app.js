870fa41bff6ce2b52fb8cc497bd8082939b27ff3
Andrew Wang <115036225+drew-gnaw@users.noreply.github.com>
2025-07-27 17:09:00 -0700

    initialize `npm run build`

--- vtn-express/app.js
+++ vtn-express/app.js
@@ -1,41 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const resources_route_1 = __importDefault(require("./public/routes/resources.route"));
+// Create the express app
+const app = (0, express_1.default)();
+app.use("/resources", resources_route_1.default);
+const server = app.listen(4321, () => {
+    console.log('App is running at http://localhost:4321');
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+server.on('error', (err) => {
+    console.error('Server failed to start:', err);
 });
-
-module.exports = app;
