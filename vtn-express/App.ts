dc3156f817c1e57bbfb1ee62406658e06f29eddb
Andrew Wang <115036225+drew-gnaw@users.noreply.github.com>
2025-09-14 14:40:05 -0700

    allow local running with npm run start

---
+++ vtn-express/App.ts
@@ -0,0 +1,12 @@
+const express = require("express");
+import resourcesHandler from "./api/resources";
+
+const app = express();
+app.use(express.json());
+
+// Mount your handlers
+app.all("/api/resources", (req: any, res: any) => resourcesHandler(req, res));
+
+app.listen(3000, () => {
+    console.log("Server running → http://localhost:3000/api/resources");
+});
