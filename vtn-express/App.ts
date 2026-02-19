293d6c6f89085f69512b30249c4786766b5446b1
Andrew Wang <115036225+drew-gnaw@users.noreply.github.com>
2026-02-18 19:04:16 -0800

    signed jwt

--- vtn-express/App.ts
+++ vtn-express/App.ts
@@ -1,11 +1,14 @@
 const express = require("express");
 import resourcesHandler from "./api/resources";
+import authHandler from "./api/auth";
 
 const app = express();
 app.use(express.json());
 
 // Mount handlers
 app.all("/api/resources", (req: any, res: any) => resourcesHandler(req, res));
+
+app.post("/api/auth/login", (req: any, res: any) => authHandler(req, res));
 
 app.listen(3000, () => {
     console.log("Server running → http://localhost:3000/api/resources");
